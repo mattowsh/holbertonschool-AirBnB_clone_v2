c25025a9deb4b3b12463062ad4755ee738c0cd0c
mattowsh <mattobelen@gmail.com>
2022-11-22 11:55:25 -0300

    task 3: contemplated the error 1290 case

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,10 @@
 -- Script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
+-- in case of ERROR 1290 (HY000): The MySQL server is running with the
+-- skip-grant-tables option, descomment the next instruction:
+--FLUSH PRIVILEGES;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
