fb24eb9ec8f2d353ed0c7fc3a46bee5eb1135b94
mattowsh <mattobelen@gmail.com>
2022-11-24 23:04:57 -0300

    task 6 advanced: reload, close and documentation. next steps: all function and update init

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -18,33 +18,56 @@ from models.place import Place
 from models.review import Review
 
 
-
 class DBStorage():
     """New Database"""
     __engine = None
     __session = None
 
-    
-    def __init__(self):
-         """init method"""
-        self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
-                                        .format(getenv('HBNB_MYSQL_USER'),
-                                                getenv('HBNB_MYSQL_PWD'),
-                                                getenv('HBNB_MYSQL_HOST'),
-                                                getenv('HBNB_MYSQL_DB')),
-                                        pool_pre_ping=True)
+    our_classes = {"User": User, "State": State, "City": City,
+                   "Amenity": Amenity, "Place": Place, "Review": Review}
 
+    def __init__(self):
+        """init method"""
+        self.__engine = create_engine(('mysql+mysqldb://{}:{}@{}/{}')
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Return all objects in the current database session"""
+        result = {}
+    
 
     def new(self, obj):
-        """new method"""
+        """Adds a new object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """save method"""
+        """Commits and save the new object to the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete method"""
+        """Deletes an object to the current database session"""
         if obj == None:
             self.__session.delete(obj)
             self.save()
+
+    def reload(self):
+        """Create all tables in the database create the current database
+        session (self.__session) from the engine (self.__engine) by using
+        a sessionmaker"""
+        Base.metadata.create_all(self.__engine)
+        new_session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+
+        # We use scoped_session for thread safety: make sure your Session
+        # is thread-safe:
+        Session = scoped_session(new_session)
+        self.__session = Session
+
+    def close(self):
+        """To close our current database session"""
+        self.__session.close()
