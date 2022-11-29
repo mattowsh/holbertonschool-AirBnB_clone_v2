86dcdbabaa55e4abb9794ce7853e20512e02b0ff
mattowsh <mattobelen@gmail.com>
2022-11-29 15:31:58 -0300

    task 1 version 0

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 1: script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello(strict_slashes=False):
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb(strict_slashes=False):
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
