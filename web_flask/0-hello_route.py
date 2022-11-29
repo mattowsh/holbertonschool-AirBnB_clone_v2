1919ba88f9c03d97afd680231276b13637e8a216
mattowsh <mattobelen@gmail.com>
2022-11-29 15:20:54 -0300

    task 0: pycodestyle was fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello(strict_slashes=False):
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
