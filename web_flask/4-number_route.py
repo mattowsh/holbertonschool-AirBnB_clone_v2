5921ab86e4b7a2554101802b889959ea14db48cf
mattowsh <mattobelen@gmail.com>
2022-11-29 21:21:49 -0300

    task 4 version 0

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Task 4: script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def print_int(n):
+    return f'{n} is a number'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
