c254149cab2999cf73efbb30c4c25661e4b0cc5c
mattowsh <mattobelen@gmail.com>
2022-11-30 16:59:15 -0300

    task 7 version 0, state

--- models/state.py
+++ models/state.py
@@ -9,7 +9,21 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
 
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                                cascade="all, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+
+            results = []
+            for element in storage.all(City).values():
+                if self.id == element.state_id:
+                    results.append(storage.all(City)[element])
+            return results
