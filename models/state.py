cd3d8ae2e91e93e3d71124b3b574fa4dd976325d
mattowsh <mattobelen@gmail.com>
2022-11-30 17:07:56 -0300

    task 7. new try with os.environ

--- models/state.py
+++ models/state.py
@@ -3,12 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
+# from os import getenv
+STRG = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if STRG == 'db':
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
