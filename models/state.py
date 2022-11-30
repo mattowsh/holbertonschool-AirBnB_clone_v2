0fb7c27ec57cca88989c99653a56ddea67c79224
mattowsh <mattobelen@gmail.com>
2022-11-30 17:04:16 -0300

    task 7 states fixed? again, new try

--- models/state.py
+++ models/state.py
@@ -12,8 +12,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                            cascade="all, delete-orphan")
+        cities = relationship("City", backref="state", cascade="delete")
     else:
         name = ""
 
@@ -28,13 +27,9 @@ class State(BaseModel, Base):
         #             results.append(storage.all(City)[element])
         #     return results
 
-    if (getenv("HBNB_TYPE_STORAGE") != "db"):
         @property
         def cities(self):
-            """cities"""
-            from models import storage
-            stacit = []
-            for stat in storage.all(City).values():
-                if self.id == city.state_id:
-                    stacit.append(storage.all(City)[stat])
-            return stacit
+            """ Getter method """
+            from models.city import City
+            return [obj for obj in models.storage.all(City).values() if
+                    obj.state_id == self.id]
