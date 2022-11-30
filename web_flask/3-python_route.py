2dba7e5e349274e85ae692a6a1320f802bb936a0
mattowsh <mattobelen@gmail.com>
2022-11-29 21:09:36 -0300

    task 2 and 3. documentation

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Task 3: script that starts a Flask web application
-"""
+"""Task 3: script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
