b7003116f63a1e6319583198f98a1216d3cd2038
mattowsh <mattobelen@gmail.com>
2022-11-29 21:09:03 -0300

    task 2 and 3. documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 2: script that starts a Flask web application
+""" Task 2: script that starts a Flask web application
 """
 from flask import Flask
 
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 0: script that starts a Flask web application
+""" Task 3: script that starts a Flask web application
 """
 from flask import Flask
 
