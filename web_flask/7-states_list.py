10bee9cbfef538223bbec4460de3a6dcbe489a61
mattowsh <mattobelen@gmail.com>
2022-12-01 13:35:55 -0300

    task 8 new version with data in 7-dump

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,15 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_appcontext(strict_slashes=False)
+@app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    return render_template('7-states_list.html', states=storage.all('State').values())
+    return render_template('7-states_list.html',
+                            states=storage.all('State').values())
 
 
 if __name__ == "__main__":
