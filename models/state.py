c0a7ab73d4e20ee07bd8e8df6775ddb9cb61d679
mattowsh <mattobelen@gmail.com>
2022-12-02 17:42:36 -0300

    task 7. state model new try

--- models/state.py
+++ models/state.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+from models.base_model import Base
 from os import getenv
+from sqlalchemy import Column, String
+
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if (getenv("HBNB_TYPE_STORAGE") == "db"):
-        cities = relationship("City", backref="state", cascade="all, \
-                              delete-orphan")
-    else:
-        name = ""
 
-    def __init__(self, *args, **kwargs):
-        """init state"""
-        super().__init__(*args, **kwargs)
-        
-    if (getenv("HBNB_TYPE_STORAGE") != "db"):
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        from sqlalchemy.orm import relationship
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    else:
+
         @property
         def cities(self):
-            """cities"""
-            from models import storage
+            from models.__init__ import storage
             from models.city import City
+
             result = []
-            for key in storage.all(City).values():
-                if self.id == key.state_id:
-                    result.append(storage.all(City)[key])
+            for element in storage.all(City).values():
+                if element.state_id == self.id:
+                    result.append(element)
             return result
 
     # if getenv("HBNB_TYPE_STORAGE") == "db":
