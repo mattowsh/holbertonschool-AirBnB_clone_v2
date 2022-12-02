4a655d01612c5c743e680f7a38539f87c1599e2e
mattowsh <mattobelen@gmail.com>
2022-12-01 22:18:45 -0300

    task 10. documentation, isma

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 9. script that starts a Flask web application
-"""
+""" task 8 """
 from flask import Flask, render_template
 from models import storage
 
@@ -10,27 +8,65 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Removes the current SQLAlchemy Session"""
+    """ removes the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states', defaults={'id': None}, strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def state_list(id):
-    """Render all states or a unique state by id"""
-    states = storage.all('State')
+def states(id=None):
+    states = storage.all("State")
     if id is None:
         return render_template(
-        '9-states.html', states=states.values(), qty=len(states)
+            '9-states.html',
+            states=storage.all('State').values(),
+            len=len(states)
         )
+    key = "State.{}".format(id)
+    states = states[key] if key in states.keys() else None
+    return render_template(
+        '9-states.html',
+        states=states,
+        len=1
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
+
+# #!/usr/bin/python3
+# """
+# Task 9. script that starts a Flask web application
+# """
+# from flask import Flask, render_template
+# from models import storage
+
+# app = Flask(__name__)
+
+
+# @app.teardown_appcontext
+# def teardown_db(exception):
+#     """Removes the current SQLAlchemy Session"""
+#     storage.close()
+
+
+# @app.route('/states', defaults={'id': None}, strict_slashes=False)
+# @app.route('/states/<id>', strict_slashes=False)
+# def state_list(id):
+#     """Render all states or a unique state by id"""
+#     states = storage.all('State')
+#     if id is None:
+#         return render_template(
+#         '9-states.html', states=states.values(), qty=len(states)
+#         )
     
-    key = 'State.{}'.format(id)
-    if key in states.keys():
-        states = states[key]
-    else:
-        states = None
-    return render_template('9-states.html', states=states, qty=1)
+#     key = 'State.{}'.format(id)
+#     if key in states.keys():
+#         states = states[key]
+#     else:
+#         states = None
+#     return render_template('9-states.html', states=states, qty=1)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
