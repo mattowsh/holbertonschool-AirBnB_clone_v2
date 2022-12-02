0ee71f1a4e999b5da1af81ccd374d9c6c2bad2b7
mattowsh <mattobelen@gmail.com>
2022-12-01 22:14:03 -0300

    task 7. documentation, isma

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Task 8: script that starts a Flask web application
-"""
+""" ni idea """
 from flask import Flask, render_template
 from models import storage
 
@@ -24,3 +22,31 @@ def states_list():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
+
+# #!/usr/bin/python3
+# """
+# Task 8: script that starts a Flask web application
+# """
+# from flask import Flask, render_template
+# from models import storage
+
+# app = Flask(__name__)
+
+
+# @app.teardown_appcontext
+# def teardown_db(exception):
+#     """ removes the current SQLAlchemy Session"""
+#     storage.close()
+
+
+# @app.route('/states_list', strict_slashes=False)
+# def states_list():
+#     """ renders all states """
+#     return render_template(
+#         '7-states_list.html', states=storage.all("State").values()
+#         )
+
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
