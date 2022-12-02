1f74286c57287136158d5e0adbd3a7f64678ebba
mattowsh <mattobelen@gmail.com>
2022-12-02 17:34:42 -0300

    task 7. state model new try

--- models/state.py
+++ models/state.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
-import os
-STRG = os.environ.get('HBNB_TYPE_STORAGE')
-
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
-    if STRG == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
+        cities = relationship("City", backref="state", cascade="all, \
+                              delete-orphan")
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """init state"""
+        super().__init__(*args, **kwargs)
+        
+    if (getenv("HBNB_TYPE_STORAGE") != "db"):
         @property
         def cities(self):
-            """ Getter method """
+            """cities"""
+            from models import storage
             from models.city import City
-            return [obj for obj in models.storage.all(City).values() if
-                    obj.state_id == self.id]
+            result = []
+            for key in storage.all(City).values():
+                if self.id == key.state_id:
+                    result.append(storage.all(City)[key])
+            return result
 
     # if getenv("HBNB_TYPE_STORAGE") == "db":
     #     __tablename__ = "states"
