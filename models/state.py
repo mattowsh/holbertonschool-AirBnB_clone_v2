7b2336b49309d220ebd9effee153d99c0ada04ed
mattowsh <mattobelen@gmail.com>
2022-12-02 17:53:04 -0300

    state model pep8 fixed

--- models/state.py
+++ models/state.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
